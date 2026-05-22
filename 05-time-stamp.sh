d32e3085b11c09964bb83619881c24a9c032863d
vivektenali <vivektenali@gmail.com>
2026-05-22 23:15:03 +0530

    file updated

--- 05-time-stamp.sh
+++ 05-time-stamp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-START_TIME=$(date+%s)
+START_TIME=$(date +%s)
 
 sleep 10
 
-END_TIME=$(date+%s)
+END_TIME=$(date +%s)
 
-TOTAL_TIME=((END_TIME-START_TIME))
+TOTAL_TIME=((END_TIME - START_TIME))
 
 echo script executed in $TOTAL_TIME seconds
